59e8f0aad79a4c2c7ef01069058eb8e5f0c4bed4
frozen-lake <arigilrain@gmail.com>
2026-02-11 21:44:23 -0600

    Implement basic transposition table functions

--- src/transposition_table.c
+++ src/transposition_table.c
@@ -7,12 +7,13 @@ void table_add(TranspositionTable* table, uint64_t key, Move move, int score, in
     int index = key & (table->size - 1);
 
     TranspositionTableEntry* entry = &table->entries[index];
-    if((entry->key == 0) || (entry->depth <= depth)){
+    if((entry->key == 0) || (entry->depth < depth) || (entry->age < table->age)){
         entry->key = key;
         entry->score = score;
         entry->depth = depth;
         entry->best_move = move;
         entry->flag = flag;
+        entry->age = table->age;
     }
 }
 
@@ -26,12 +27,25 @@ TranspositionTableEntry* table_get(TranspositionTable* table, uint64_t key){
     return NULL;
 }
 
-/* Set all fields of all entries to 0 */
 void table_clear(TranspositionTable* table){
     memset(table->entries, 0, (table->size * sizeof(TranspositionTableEntry)));
+    table->age = 0;
 }
 
-/* Allocate n megabytes of memory to this table */
 void table_init(TranspositionTable* table){
+    table->entries = (TranspositionTableEntry*) calloc(TT_ENTRIES, sizeof(TranspositionTableEntry));
+    if(table->entries == NULL){
+        table->size = 0;
+        return;
+    }
     table->size = TT_ENTRIES;
+    table->age = 0;
+}
+
+void table_free(TranspositionTable* table){
+    if(table->entries){
+        free(table->entries);
+        table->entries = NULL;
+    }
+    table->size = 0;
 }
--- src/transposition_table.h
+++ src/transposition_table.h
@@ -2,7 +2,12 @@
 
 #ifndef TTABLE_H
 #define TTABLE_H
-#define TT_ENTRIES 1048576 // ~129 MB
+#define TT_ENTRIES 1048576 // 2^20
+
+// Transposition table entry flags
+#define TT_EXACT 0
+#define TT_LOWER 1
+#define TT_UPPER 2
 
 typedef struct {
     uint64_t key;
@@ -10,12 +15,13 @@ typedef struct {
     int16_t score;
     uint8_t depth;
     uint8_t flag;
+    uint8_t age;
 } TranspositionTableEntry;
 
 typedef struct {
     TranspositionTableEntry* entries;
-    uint8_t* locks;
     int size;
+    uint8_t age;
 } TranspositionTable;
 
 #endif
@@ -24,3 +30,4 @@ void table_add(TranspositionTable* table, uint64_t key, Move move, int score, in
 void table_clear(TranspositionTable* table);
 TranspositionTableEntry* table_get(TranspositionTable* table, uint64_t key);
 void table_init(TranspositionTable* table);
+void table_free(TranspositionTable* table);
